fb9c551d3a695a281ccecb3f302c0f846991f8c6
stnrfaizahh <nurfaizahh234@gmail.com>
2024-08-11 17:17:44 +0700

    modifikasi tampilan awal

---
+++ MVC/app/views/welcome.blade.php
@@ -0,0 +1,13 @@
+<!DOCTYPE html>
+<html lang="{{str_replace('_', '_', app()->getLocale())}}">
+    <head>
+        <meta charset="UTF-8">
+        <meta name="viewport" content="width=device-width, initial=scale=1.0">
+        <meta http-equiv="X-UA-Compatible" content="ie=edge">
+        <title>VSGA</title>
+    </head>
+    <body>
+        <h2> Ini adalah percobaan</h2>
+        <h3> Semangat belajar</h3>
+    </body>
+</html>
